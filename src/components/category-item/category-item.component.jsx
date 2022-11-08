3126d39a60562bbb0cb0092018db4076387bff97
Marko Milosavljevic <srbobor@yahoo.com>
2022-11-08 21:58:16 +0100

    reverting change

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -8,7 +8,7 @@ const CategoryItem = ({ category }) => {
             <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
             <div className="category-body-container">
             <h2>{title}</h2>
-            <p>Shlop Now</p>
+            <p>Shop Now</p>
             </div>
         </div>
     )
